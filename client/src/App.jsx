2cfa2d1b19000c6161683e05f9451f6e96f89339
KyeongsooLee <leeksca26@gmail.com>
2023-03-17 14:29:13 -0400

    Added About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
-import { Home, CreatePost } from './pages';
+import { Home, CreatePost, About } from './pages';
 
 const App = () => {
   return (
@@ -11,13 +11,16 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
-
-        <Link to ="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+        <div className="flex items-center space-x-4">
+          <Link to ="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+          <Link to ="/About" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">About</Link>
+        </div>
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="max-w-7xl mx-auto">
+          <div>
+            <h1 className="font-extrabold text-[#222328] text-[32px]">About</h1>
+            <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]"> 
+              Welcome to my website! I'm a Full-Stack Developer with a passion for creating innovative web applications that enhance user experience. I specialize in developing web-based software using a variety of programming languages and frameworks.<br /><br />
+
+              My focus is on building scalable, maintainable, and efficient applications that meet the needs of clients and end-users alike.<br /><br />
+
+              My technical skills include proficiency in front-end technologies such as HTML, CSS, and JavaScript, as well as expertise in back-end languages like Python, Java, Go, and Node.js. I have experience working with databases such as SQL and NoSQL, and I am comfortable with cloud-based technologies such as AWS and Azure.<br /><br />
+
+              In addition to my technical skills, I pride myself on my ability to collaborate with others and communicate effectively. I believe that teamwork and communication are key to creating successful software solutions.<br /><br />
+
+              When I'm not coding, I enjoy hiking, reading, and spending time with my family. I am always looking for new challenges and opportunities to grow, and I'm excited about the future of the technology industry and the role that I can play in shaping it.<br /><br />
+
+              Thank you for taking the time to visit my website, and I look forward to connecting with you!</p>
+          </div>
+    
+        </section>
+      )
+}
+
+export default About
